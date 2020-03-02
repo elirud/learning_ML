e87207d86939856ae07d9a7ed42a12d9741a8c7d
Elias Rudberg <eli.rud@hotmail.com>
2020-03-03 00:02:21 +0100

    Add model evaluation

--- iris_flowers.py
+++ iris_flowers.py
@@ -45,3 +45,12 @@ models.append(('KNN', KNeighborsClassifier()))
 models.append(('CART', DecisionTreeClassifier()))
 models.append(('NB', GaussianNB()))
 models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+	results.append(cv_results)
+	names.append(name)
+	print(f'{name}: {cv_results.mean():.5f} ({cv_results.std():.3f})')
